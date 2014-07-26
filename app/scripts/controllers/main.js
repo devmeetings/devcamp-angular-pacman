43b0387d1e178e317f713873e1c1261f27f018ec
Wojciech Łakomy <wojtek.lak@gmail.com>
2014-07-26 15:47:46 +0200

    fix

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,10 @@
  * Controller of the pacmanApp
  */
 angular.module('pacmanApp')
-  .controller('MainCtrl', ['$scope', function ($scope) {
-
-
+  .controller('MainCtrl', ['$scope', 'DEFAULT_PLAYERS', function ($scope, DEFAULT_PLAYERS) {
+    $scope.$watch(function () {
+        return DEFAULT_PLAYERS;
+      }, function(nV) { console.log('players', nV); },
+      true
+    );
   }]);
--- app/scripts/directives/board-directive.js
+++ app/scripts/directives/board-directive.js
@@ -1,20 +1,19 @@
-app.directive('board', ['boardService', 'PLAYERS_ACTIONS', function(boardService, PLAYERS_ACTIONS) {
+app.directive('board', ['boardService', 'PLAYERS_ACTIONS', '$timeout', function(boardService, PLAYERS_ACTIONS, $timeout) {
   var moveKeyCode = PLAYERS_ACTIONS;
   return {
     restrict: 'EC',
     templateUrl: '/views/board.html',
     link: function($scope, $element, $attrs) {
       $scope.board = boardService.returnBoard();
-      console.log($scope.board.length);
-      console.log($scope.board[0].length);
 
       $('html').on('keydown', function(e) {
-        e.preventDefault();
-        if (!moveKeyCode[e.keyCode]) return;
-        var changesArray = boardService.move(moveKeyCode[e.keyCode].playerId, moveKeyCode[e.keyCode].action);
-        changesArray.forEach(function(item) {
-          drawNewStatus(item);
-        });
+        $timeout(function() {
+          if (!moveKeyCode[e.keyCode]) return;
+          var changesArray = boardService.move(moveKeyCode[e.keyCode].playerId, moveKeyCode[e.keyCode].action);
+          changesArray.forEach(function(item) {
+            drawNewStatus(item);
+          });
+        }, 0);
       });
 
       // statusObject structure: { x: integer, y: integer, status: integer }
