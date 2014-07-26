8f9da770eedb77eaf8ecfdfd5af96e09fbef26e5
zuza <zuza@zuzas-MacBook-Pro.local>
2014-07-26 12:20:36 +0200

    merge

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,6 +8,49 @@
  * Controller of the pacmanApp
  */
 angular.module('pacmanApp')
-  .controller('MainCtrl', ['$scope', function ($scope) {
-  	//boardService.move();
-  }]);
+
+  .controller('MainCtrl', function ($scope, $goQuery, $goUsers) {
+  	
+  	$scope.showMoves = function () {
+  		$scope.moves = $goQuery('/moves', {});
+  		$scope.moves.$sync()
+  	}
+
+  	$scope.showPlayers = function () {
+  		$scope.players = $goUsers();
+  		$scope.players.$sync();
+  	}
+
+
+  	$scope.move = function () {
+  		$scope.players.$self().then(function(user){
+	  		$scope.moves.$add({
+	  			x: $scope.nextMove.x,
+	  			y: $scope.nextMove.y,
+	  			user: user.id
+	  		}).then(function() {
+	  			$scope.$apply(function() {
+	  				$scope.nextMove = null;
+	  			});
+	  		});
+  		});
+
+  	};  	
+
+  	$scope.showMoves();
+  	$scope.showPlayers();
+
+  })
+  .filter('isActivePlayer', function() {
+  	return function(input, players) {
+  		var result = [];
+  		angular.forEach(input.$omit(), function(move){
+  			if (!!players[move.user]) {
+  				result.push(move);
+  			}
+  		});
+
+  		return result;
+  	};
+  })
+
